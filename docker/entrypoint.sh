90eab0ad409ba2b353b2446f412ad48916eed448
estr05 <estradaperezneyser@gmail.com>
2026-05-22 19:23:01 -0600

    fix: dynamically assign Nginx port from PORT env

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -10,6 +10,12 @@ php artisan view:cache
 echo "Running migrations..."
 php artisan migrate --force
 
+# Adjust Nginx port dynamically if $PORT is set by the hosting platform
+if [ -n "$PORT" ]; then
+  echo "Adjusting Nginx port to $PORT..."
+  sed -i "s/80/${PORT}/g" /etc/nginx/http.d/default.conf
+fi
+
 # Start Supervisor
 echo "Starting Supervisor..."
 exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
